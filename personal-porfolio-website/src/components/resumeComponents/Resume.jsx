f0a8cb4aae39959adc8219d2dfa5fabb9d36af19
Manny Fong <mafong081102@gmail.com>
2024-03-19 21:51:40 -0400

    Fixed resume responsiveness

--- personal-porfolio-website/src/components/resumeComponents/Resume.jsx
+++ personal-porfolio-website/src/components/resumeComponents/Resume.jsx
@@ -30,10 +30,10 @@ export default function Resume() {
         </Row>
 
         <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
+          <Document file={pdf} className="d-flex justify-content-center !overflow-hidden">
             <Page
               pageNumber={1}
-              scale={width > 200 ? 1.7 : 0.6}
+              scale={width > 786  ? 1.7 : 0.6}
               renderAnnotationLayer={false}
               renderTextLayer={false}
             />
